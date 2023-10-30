815f5601850658221f94f9f09f8aaafc363032ad
MANAS TAMBE <manastambe27@gmail.com>
2023-10-30 10:22:45 +0530

    Updated Fronte-end

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/auth.js";
 const Dashboard = () => {
   const [auth] = useAuth();
   return (
-    <Layout title={"Dashboard - Ecommerce App"}>
+    <Layout title={"Dashboard - ECommerce App"}>
       <div className="container-flui m-3 p-3">
         <div className="row">
           <div className="col-md-3">
